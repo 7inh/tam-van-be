73bfe9e2f9330c66a791bb066d86ab1c48febeb2
7inh <tranquoclinh@gmail.com>
2024-01-28 20:30:54 +0700

    Remove commented out code in viettelPostInstance.ts

--- src/common/viettelPostInstance.ts
+++ src/common/viettelPostInstance.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const viettelPostInstance = axios.create({
     baseURL: "https://partner.viettelpost.vn/v2",
     timeout: 15000,
-    // withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
